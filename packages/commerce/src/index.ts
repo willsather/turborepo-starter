ae52461cd3dfa5558ed55a71934f673c13935e7b
Will Sather <satherw@vmware.com>
2024-02-29 14:50:02 -0500

    fix: tsup watch config

--- packages/commerce/src/index.ts
+++ packages/commerce/src/index.ts
@@ -1,6 +1,1 @@
-import { Product } from "./products/Product";
-import ProductService from "./products/ProductService";
-import CustomProductService from "./products/CustomProductService";
-
-export { type Product, type ProductService };
-export { CustomProductService };
+export * from "./products";
---
+++ packages/commerce/src/products/index.ts
@@ -0,0 +1,6 @@
+import { Product } from "./Product";
+import ProductService from "./ProductService";
+import CustomProductService from "./CustomProductService";
+
+export { type Product, type ProductService };
+export { CustomProductService };
