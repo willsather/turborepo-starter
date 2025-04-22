ecfaacfa9405f43c840b4ebb068abb129501845e
willsather <will@sather.ws>
2025-04-21 22:31:26 -0400

    fix: button opacity

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -11,13 +11,13 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-black/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/75",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/75",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/75",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
